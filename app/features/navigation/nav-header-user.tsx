fdddd10b83b7220750e03f6009e973936f2aa832
Jordan Kahtava <j.kahtava@cityssm.on.ca>
2026-07-27 14:26:47 -0400

    moved profile error button to sign-out-button so I can reuse the same sign out function

--- app/features/navigation/nav-header-user.tsx
+++ app/features/navigation/nav-header-user.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from '@/components/ui/sidebar';
-import { SignOutMenuItem } from '../../../components/sign-out-button';
+import { SignOutMenuItem, SignOutProfileError } from '../../../components/sign-out-button';
 import { Skeleton } from '../../../components/ui/skeleton';
 import { Switch } from '../../../components/ui/switch';
 import { useTheme } from 'next-themes';
@@ -76,30 +76,7 @@ export function NavUser({ session, isPending }: { session: Session; isPending: b
   }
 
   if (!user) {
-    return (
-      <SidebarMenuButton
-        size="lg"
-        className="w-56 rounded-lg border border-destructive/20 bg-destructive/5 hover:bg-destructive/10 transition-colors group"
-      >
-        <div className="relative">
-          <CircleUserRound className="h-7 w-7 text-destructive/40 group-hover:scale-105 transition-transform group-hover:text-destructive " />
-          <span className="absolute -top-0.5 -right-0.5 flex h-2 w-2">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-destructive opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-2 w-2 bg-destructive"></span>
-          </span>
-        </div>
-
-        <div className="grid flex-1 text-left text-sm leading-tight">
-          <span className="font-semibold text-destructive/60 tracking-tight group-hover:text-destructive">Logout</span>
-          <span className="text-xs text-destructive/60 flex items-center gap-1 group-hover:text-destructive">
-            <span>Profile Error</span>
-          </span>
-        </div>
-        <div className="relative">
-          <LogOut className="h-5 w-5 text-destructive/40 scale-90 group-hover:scale-100 group-hover:text-destructive transition-all  duration-200 ease-out  " />
-        </div>
-      </SidebarMenuButton>
-    );
+    return <SignOutProfileError />;
   }
 
   return (
--- components/sign-out-button.tsx
+++ components/sign-out-button.tsx
@@ -4,7 +4,7 @@ import { signOut } from '@/lib/auth-client';
 
 import { redirect, usePathname } from 'next/navigation';
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
-import { LogOut } from 'lucide-react';
+import { CircleUserRound, LogOut } from 'lucide-react';
 import { SidebarMenuButton } from './ui/sidebar';
 
 const handleLogOut = (pathname: string) => {
@@ -35,6 +35,36 @@ export function SignOutMenuButton() {
     <SidebarMenuButton onClick={() => handleLogOut(pathname)}>
       <LogOut />
       Logout
+    </SidebarMenuButton>
+  );
+}
+
+export function SignOutProfileError() {
+  const pathname = usePathname();
+
+  return (
+    <SidebarMenuButton
+      size="lg"
+      className="w-56 rounded-lg border border-destructive/20 bg-destructive/5 hover:bg-destructive/10 transition-colors group"
+      onClick={() => handleLogOut(pathname)}
+    >
+      <div className="relative">
+        <CircleUserRound className="h-7 w-7 text-destructive/40 group-hover:scale-105 transition-transform group-hover:text-destructive " />
+        <span className="absolute -top-0.5 -right-0.5 flex h-2 w-2">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-destructive opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-2 w-2 bg-destructive"></span>
+        </span>
+      </div>
+
+      <div className="grid flex-1 text-left text-sm leading-tight">
+        <span className="font-semibold text-destructive/60 tracking-tight group-hover:text-destructive">Logout</span>
+        <span className="text-xs text-destructive/60 flex items-center gap-1 group-hover:text-destructive">
+          <span>Profile Error</span>
+        </span>
+      </div>
+      <div className="relative">
+        <LogOut className="h-5 w-5 text-destructive/40 scale-90 group-hover:scale-100 group-hover:text-destructive transition-all  duration-200 ease-out  " />
+      </div>
     </SidebarMenuButton>
   );
 }
